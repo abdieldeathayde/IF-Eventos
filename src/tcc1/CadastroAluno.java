ad4d4c8bd9d11ce4bf18ef409844c163b07e3cea
Athay <abdieldeathayde67@gmail.com>
2024-11-11 19:26:35 -0300

    feat: adicionando validacao dos campos telefone e email

--- src/tcc1/CadastroAluno.java
+++ src/tcc1/CadastroAluno.java
@@ -42,7 +42,7 @@ public class CadastroAluno extends JFrame {
 	private JPasswordField senhaTF1;
 	private JFormattedTextField emailTF;
 	private JFormattedTextField telefoneJFTF;
-	private JLabel usuarioJLabel, campoEmailJLBL, telefoneJLabel; 
+	private JLabel campoUsuarioJLabel, campoEmailJLabel, telefoneJLabel; 
 
 	/**
 	 * * Launch the application.
@@ -92,19 +92,13 @@ public class CadastroAluno extends JFrame {
 		textoIncricaoTF.setBackground(Color.BLUE);
 		contentPane.add(textoIncricaoTF);
 
-		JLabel campoUsuarioJLBL = new JLabel("Usuário:");
-		campoUsuarioJLBL.setBounds(44, 201, 45, 13);
-		campoUsuarioJLBL.setFont(new Font("Tahoma", Font.BOLD, 10));
-		contentPane.add(campoUsuarioJLBL);
+		
 
 		usuarioTF = new JTextField();
 		usuarioTF.setBounds(191, 199, 266, 19);
 		contentPane.add(usuarioTF);
 		usuarioTF.setColumns(10);
-		
-		String usuario = usuarioTF.toString();
-		boolean isUserValid = usuario.matches("^[a-zA-Z]{3,9}$");
-		System.out.println(isUserValid ? "Usuário válido" : "Usuário inválido");
+
 
 		JLabel campoSenhaJLBL = new JLabel("Senha:");
 		campoSenhaJLBL.setBounds(44, 246, 45, 13);
@@ -115,56 +109,67 @@ public class CadastroAluno extends JFrame {
 		senhaTF1.setBounds(191, 244, 266, 19);
 		contentPane.add(senhaTF1);
 
-		JLabel campoEmailJLBL = new JLabel("Email:");
-		campoEmailJLBL.setBounds(44, 300, 45, 13);
-		contentPane.add(campoEmailJLBL);
+		
 		
 		
 //		String email = "exemplo@aluno.ifsc.edu.br";
+		//MascaraEmail("****************@aluno.ifsc.edu.br")
 		
 		
-		emailTF = new JFormattedTextField(MascaraEmail("****************@aluno.ifsc.edu.br"));
+		emailTF = new JFormattedTextField();
 		emailTF.setBounds(191, 300, 266, 19);
 		contentPane.add(emailTF);
 		emailTF.setColumns(10);
 		
 		String email = emailTF.toString();
 		boolean isValidEmail = email.matches("^[\\w\\.-]+@[a-zA-Z\\d\\.-]+\\.[a-zA-Z]{2,6}$");
-		System.out.println(isValidEmail ? "Email válido" : "Email inválido");
+//		System.out.println(isValidEmail ? "Email válido" : "Email inválido");
+		
 
+		telefoneJFTF = new JFormattedTextField();
+		telefoneJFTF.setToolTipText("");
+		telefoneJFTF.setBounds(191, 340, 266, 19);
+		contentPane.add(telefoneJFTF);
+		telefoneJFTF.setColumns(10);
+		
+		
+
+		
+		
+		JButton botaoInscricaoJButton = new JButton("Inscrever");
+		botaoInscricaoJButton.setBounds(125, 400, 111, 21);
+		botaoInscricaoJButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					validarCampos();
+					inserirAluno();
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				dispose();
+			}
+		});
+		JLabel campoUsuarioJLBL = new JLabel("Usuário:");
+		campoUsuarioJLBL.setBounds(44, 201, 45, 13);
+		campoUsuarioJLBL.setFont(new Font("Tahoma", Font.BOLD, 10));
+		contentPane.add(campoUsuarioJLBL);
+		
+		JLabel campoEmailJLBL = new JLabel("Email:");
+		campoEmailJLBL.setBounds(44, 300, 45, 13);
+		contentPane.add(campoEmailJLBL);
+		
 		JLabel campoTelefoneJLBL = new JLabel("Telefone");
 		campoTelefoneJLBL.setBounds(44, 340, 85, 13);
 		campoTelefoneJLBL.setHorizontalAlignment(SwingConstants.LEFT);
 		contentPane.add(campoTelefoneJLBL);
 		
 		
-
-		telefoneJFTF = new JFormattedTextField(Mascara("(***) ****-****"));
-		telefoneJFTF.setToolTipText("");
-		telefoneJFTF.setBounds(191, 340, 266, 19);
-		contentPane.add(telefoneJFTF);
-		telefoneJFTF.setColumns(10);
-		
-		String telefone = telefoneJFTF.toString();
-		boolean isValidPhone = telefone.matches("^[0-9]$");
-		System.out.println(isValidPhone ? "Telefone válido" : "Telefone inválido");
 		
 		
-		JButton botaoInscricaoJButton = new JButton("Inscrever");
-		botaoInscricaoJButton.setBounds(125, 400, 111, 21);
-		botaoInscricaoJButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Login login = new Login();
-				login.setVisible(true);
-				try {
-					inserirAluno();
-				} catch (ClassNotFoundException | SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				dispose();
-			}
-		});
 		botaoInscricaoJButton.setBackground(Color.WHITE);
 		botaoInscricaoJButton.setForeground(Color.BLACK);
 		contentPane.add(botaoInscricaoJButton);
@@ -202,30 +207,40 @@ public class CadastroAluno extends JFrame {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		
 	}	
 
 	private void validarCampos() {
-		String email = emailTF.getText();
-		String telefone = telefoneJFTF.getText();
-		
-		
-		
-		campoEmail.setText(validarEmail(email) ? "Email válido" : "Email inválido");
-	}
+			//String usuario = usuarioTF.getText();
+			String email = emailTF.getText();
+			String telefone = telefoneJFTF.getText();
+			
+			
+			System.out.println(validarEmail(email) ? "Email válido" : "Email inválido");
+			System.out.println(validarTelefone(telefone) ? "Telefone válido" : "Telefone inválido");
+			
+			
+			
+			//boolean isEmailValid = email.matches("^[a-zA-Z]{3,9}$");
+			//System.out.println(isEmailValid ? "Usuário válido" : "Usuário inválido");
+			
+			
+		}
 	
-	 private boolean validarEmail(String email) {
+	 	private boolean validarEmail(String email) {
 	        String emailRegex = "^[\\w.-]+@[a-zA-Z\\d.-]+\\.[a-zA-Z]{2,6}$";
 	        return Pattern.matches(emailRegex, email);
 	    }
 
 	    private boolean validarTelefone(String phone) {
-	        String phoneRegex = "^\\(?\\d{2}\\)?[\\s-]?\\d{4,5}-\\d{4}$";
+	        String phoneRegex = "^\\(?\\d{2,9}\\)?[\\s-]?\\d{4,9}-\\d{4,9}$";
 	        return Pattern.matches(phoneRegex, phone);
 	    }
 	
 	
-	public static SimpleDateFormat getFormatData() {
-		return formatData;
+	    public static SimpleDateFormat getFormatData() {
+	    	return formatData;
 	}
 
 	public static void setFormatData(SimpleDateFormat formatData) {
